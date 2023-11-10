3c75adc3db19eb50ff3ac11cea9b6ca43c1a5f57
SandaTiluka <sanda.tiluka@gmail.com>
2023-11-10 15:31:55 +0200

    commit

---
+++ Day2_Majasdarbs/src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        task1();
+    }
+
+        public static void task1(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ievadiet savu vārdu!");
+
+        String input = sc.nextLine();
+
+
+
+
+        }
+    }
