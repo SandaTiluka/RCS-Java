67a4045fa48bf6d0293d44dfbfc7aea7bf32cf86
SandaTiluka <sanda.tiluka@gmail.com>
2023-11-10 23:46:58 +0200

    pedejas izmainas

--- Day2_Majasdarbs/src/main/java/org/example/Main.java
+++ Day2_Majasdarbs/src/main/java/org/example/Main.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        task1();
-    }
+
+        task2();}
 
         public static void task1(){
         Scanner sc = new Scanner(System.in);
@@ -19,14 +19,26 @@ public class Main {
             System.out.println("###");
             }else if(len < 10) {
             System.out.println("##");
-            } else if (len == 10) {
+            }else if(len == 10) {
             System.out.println("#");
 
         }
 
+        public static void task2(){
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("Ievadiet simbolu virkni/savu vārdu!");
+
+            String input = sc.nextLine();
+            int len = input.length();
+
+            if(len > 5 && len < 10) {
+                System.out.println("Jā");
+            }else{
+            System.out.println("Not ok");
+
+            }
 
 
 
-
-        }
     }
