1fe816a66f7fae2504be6fb1e66d7776c18efcef
SandaTiluka <sanda.tiluka@gmail.com>
2023-11-14 19:02:44 +0200

    commit

---
+++ Day3_loops/src/main/java/org/example/Main.java
@@ -0,0 +1,29 @@
+package org.example;
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        task1();
+    }
+
+    public static void task1(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ievadiet skaitli");
+        for(int i = 0; i < 7; i+5);
+        System.out.println(i);
+
+    }
+
+
+        public static void sample1(){
+            for(int i = 0; i < 5; i++){
+                System.out.println(i);
+            System.out.println("cau");
+                System.out.println("----");
+
+
+        }
+    }
+}
