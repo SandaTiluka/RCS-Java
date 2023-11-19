36912f0315721e16131bdee0163e71db322b21f8
SandaTiluka <sanda.tiluka@gmail.com>
2023-11-20 00:02:33 +0200

    com1

---
+++ Day3_MD/src/main/java/org/example/Main.java
@@ -0,0 +1,50 @@
+package org.example;
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        uzd2();
+    }
+
+        public static void uzd2(){
+            Scanner sc = new Scanner(System.in);
+
+
+
+            int input = 0;
+            int last = 0;
+            
+
+
+            while(input >= last){
+                System.out.println("Ievadiet skaitli!");
+                input = sc.nextInt();
+                System.out.println(input);
+                last = input + 0;
+
+
+                if(input < 0){
+                    break;
+                }
+
+
+            }
+
+        }
+
+
+
+        public static void uzd1(){
+            String a = "";
+
+            for (int i = 0; i < 5; i++){
+                a = a + "$";
+                System.out.println(a);
+
+            }
+
+        }
+
+    }
