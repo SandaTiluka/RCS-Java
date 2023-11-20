60c5d64636ca8faf20975fe32cdbdd775104e830
SandaTiluka <sanda.tiluka@gmail.com>
2023-11-20 19:56:58 +0200

    v

--- Day4_classes/src/main/java/org/example/Main.java
+++ Day4_classes/src/main/java/org/example/Main.java
@@ -3,12 +3,25 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        String a = "aaaa";
-        String b = "bbbb";
-        String c = "cccc";
-        sample1(a,b,c);
+//        String a = "aaaa";
+//        String b = "bbbb";
+//        String c = "cccc";
+//        sample1(a,b,c);
+        int b = 9;
+        sample2(b);
 
     }
+    public static void sample2(int b){
+        String a = "";
+
+        for (int i = 0; i < b; i++){
+            a = a + "$";
+            System.out.println(a);
+
+        }
+
+    }
+
 
     public static void sample1(String a, String b, String c){
 
