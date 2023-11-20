316a57e1d7679476a0b85c48035739c240b8fd0f
SandaTiluka <sanda.tiluka@gmail.com>
2023-11-20 18:54:52 +0200

    com

---
+++ Day4_classes/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+
+public class Main {
+    public static void main(String[] args) {
+        sample1();
+        String a : "aaaa";
+        String b : "bbbb";
+        String c : "cccc";
+
+
+    }
+
+    public static void sample1(String a, String b, String c){
+        System.out.println(c + b + a);
+    }
+}
