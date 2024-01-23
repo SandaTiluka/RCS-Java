a88006e40c1d66ee44e8dd94eb9937cb111f33e6
SandaTiluka <sanda.tiluka@gmail.com>
2024-01-23 20:45:28 +0200

    com

--- Day9-angular/src/app/atnemsana/atnemsana.component.ts
+++ Day9-angular/src/app/atnemsana/atnemsana.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-atnemsana',
   standalone: true,
-  imports: [],
+  imports: [AtnemsanaComponent, Component, NgModule,FormsModule],
   templateUrl: './atnemsana.component.html',
   styleUrl: './atnemsana.component.css'
 })
-export class AtnemsanaComponent implements OnInit {
+export class AtnemsanaComponent {
   userInput1: number;
   userInput2: number;
   result: number;
